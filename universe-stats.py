980cea23fa4e6d25c8cfe86504f368532887334c
Hannah Rosenberg <hannah@velascommerce.com>
2023-11-03 10:30:46 -0400

    adding universe-stats script.

--- universe-stats.py
+++ universe-stats.py
@@ -3,17 +3,11 @@ from settings import REST_HOST, MACAROON_PATH, TLS_PATH
 
 
 def main():
-    url = f"https://{REST_HOST}/v1/taproot-assets/universe/federation"
+    url = f"https://{REST_HOST}/v1/taproot-assets/universe/stats"
     macaroon = codecs.encode(open(MACAROON_PATH, "rb").read(), "hex")
     headers = {"Grpc-Metadata-macaroon": macaroon}
 
-    universe = {"host": "IP or URL : port"}
-
-    data = {"servers": [universe]}
-
-    response = requests.post(
-        url, headers=headers, data=json.dumps(data), verify=TLS_PATH
-    )
+    response = requests.get(url, headers=headers, verify=TLS_PATH)
 
     if response.status_code == 200:
         response_data = response.json()
